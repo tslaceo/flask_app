0d03101d0665c7577096dbc379bc6d8778d1eb02
tslaceo <77980929+tslaceo@users.noreply.github.com>
2023-09-19 20:55:06 +0300

    Update myapp.py

--- webapp/myapp.py
+++ webapp/myapp.py
@@ -72,5 +72,6 @@ def serialize(self):
 setattr(Product, "serialize", serialize)
 
 if __name__ == '__main__':
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
